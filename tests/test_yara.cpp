a231fa67e249c9c74bc06dc03d088e8a86b44d15
Karol <17812121+W3ndige@users.noreply.github.com>
2025-01-03 10:05:31 +0100

    Test the GH action for building and testing

--- tests/test_yara.cpp
+++ tests/test_yara.cpp
@@ -7,19 +7,19 @@
 TEST_CASE("Test basic Yara scan", "[Yara]") {
     Yara yara(0);
 
-    REQUIRE_NOTHROW(yara.addSourceFromFile("../tests/files/test.yara"));
+    REQUIRE_NOTHROW(yara.addSourceFromFile("../..//tests/files/test.yara"));
 
     REQUIRE_NOTHROW(yara.initScanner());
 
     SECTION("File contains a match") {
         // Test a file expected to return true
-        bool result = yara.scanFile("../tests/files/match_foo_test.txt");
+        bool result = yara.scanFile("../..//tests/files/match_foo_test.txt");
         REQUIRE(result == true);
     }
     
     SECTION("File does not contain a match") {
         // Test a file expected to return false
-        bool result = yara.scanFile("../tests/files/no_match_foo_test.txt");
+        bool result = yara.scanFile("../..//tests/files/no_match_foo_test.txt");
         REQUIRE(result == false);
     }
 }
@@ -27,30 +27,30 @@ TEST_CASE("Test basic Yara scan", "[Yara]") {
 TEST_CASE("Tests if there are any identifiers saved from the match.", "[Yara Matched Identifiers]") {
     Yara yara(0);
 
-    REQUIRE_NOTHROW(yara.addSourceFromFile("../tests/files/test.yara"));
+    REQUIRE_NOTHROW(yara.addSourceFromFile("../..//tests/files/test.yara"));
 
     REQUIRE_NOTHROW(yara.initScanner());
 
-    bool result = yara.scanFile("../tests/files/match_foo_test.txt");
+    bool result = yara.scanFile("../..//tests/files/match_foo_test.txt");
     REQUIRE(result == true);
-    REQUIRE(yara.getMatchedIdentifiersForFile("../tests/files/match_foo_test.txt").size() > 0);
+    REQUIRE(yara.getMatchedIdentifiersForFile("../..//tests/files/match_foo_test.txt").size() > 0);
 }
 
-TEST_CASE("Tests if there are none identifiers with invalid path that was never scanned..", "[Yara Matched Identifiers for Invalid Path]") {
+TEST_CASE("Tests if there are none identifiers with invalid path that was never scanned", "[Yara Matched Identifiers for Invalid Path]") {
     Yara yara(0);
 
-    REQUIRE_NOTHROW(yara.addSourceFromFile("../tests/files/test.yara"));
+    REQUIRE_NOTHROW(yara.addSourceFromFile("../..//tests/files/test.yara"));
 
     REQUIRE_NOTHROW(yara.initScanner());
 
-    bool result = yara.scanFile("../tests/files/match_foo_test.txt");
+    bool result = yara.scanFile("../..//tests/files/match_foo_test.txt");
     REQUIRE(result == true);
-    REQUIRE(yara.getMatchedIdentifiersForFile("../tests/files/match_foo.txt").size() == 0);
+    REQUIRE(yara.getMatchedIdentifiersForFile("../..//tests/files/match_foo.txt").size() == 0);
 }
 
 TEST_CASE("Test adding invalid rule", "[Yara Invalid Rule]") {
     Yara yara(0);
 
-    REQUIRE(yara.addSourceFromFile("../tests/files/test_invalid_rule.yara") == false);
+    REQUIRE(yara.addSourceFromFile("../..//tests/files/test_invalid_rule.yara") == false);
 }
 
